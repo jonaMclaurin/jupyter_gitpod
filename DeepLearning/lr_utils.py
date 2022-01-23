1575c1bd688c43f07fb42b9390485a0243e78924
Jonatan Ramírez Medina <jonatan_lqfb@hotmail.com>
2022-01-23 09:49:30 +0000

    finished first assignemnt

--- DeepLearning/lr_utils.py
+++ DeepLearning/lr_utils.py
@@ -3,11 +3,11 @@ import h5py
 import os
     
 def load_dataset():
-    train_dataset = h5py.File(r'DeepLearning/DataSets/train_catvnoncat.h5', "r")
+    train_dataset = h5py.File(r'/workspace/jupyter_gitpod/DeepLearning/DataSets/train_catvnoncat.h5', "r")
     train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
     train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
 
-    test_dataset = h5py.File(r'DeepLearning/DataSets/test_catvnoncat.h5', "r")
+    test_dataset = h5py.File(r'/workspace/jupyter_gitpod/DeepLearning/DataSets/test_catvnoncat.h5', "r")
     test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
     test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
 
